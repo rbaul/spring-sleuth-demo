16957f6a949ed809d74da36c941ba32fb424c3eb
rbaul <4RFVbgt%>
2019-12-23 08:41:05 +0200

    Initial project structure

---
+++ server1-sb2/src/main/java/com/spring/sleuth/demo/server1_sb2/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.spring.sleuth.demo.server1_sb2.config;
+
+import com.spring.sleuth.demo.server1_sb2.web.Service1Controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Value("${application.title:Service 1 (Spring Boot 2)}")
+    private String title;
+
+    @Bean
+    public Docket getSecureDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Service 1")
+                .enable(true)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(Service1Controller.class.getPackage().getName()))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title(title)
+                .build();
+    }
+
+
+}
---
+++ server1-sb2/src/main/java/com/spring/sleuth/demo/server1_sb2/services/Service1ServiceImpl.java
@@ -0,0 +1,22 @@
+package com.spring.sleuth.demo.server1_sb2.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class Service1ServiceImpl {
+
+    public void service2FlowRequest() {
+
+    }
+
+    public void service3FlowRequest() {
+
+    }
+
+    public void serviceKafkaFlowRequest() {
+    }
+}
