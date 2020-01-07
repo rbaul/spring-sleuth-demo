a4376bd979ef1dda7d4b138b71a33cd14ff08244
rbaul <4RFVbgt%>
2020-01-07 15:20:06 +0200

    Configure default Sampler to AlwaysSampler for export true always

--- server3-sb1/src/main/java/com/spring/sleuth/demo/server3_sb1/config/ServerConfig.java
+++ server3-sb1/src/main/java/com/spring/sleuth/demo/server3_sb1/config/ServerConfig.java
@@ -3,9 +3,17 @@ package com.spring.sleuth.demo.server3_sb1.config;
 import com.spring.sleuth.demo.server3_sb1.services.Server1ClientImpl;
 import com.spring.sleuth.demo.server3_sb1.services.Server2ClientImpl;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(clients = {Server1ClientImpl.class, Server2ClientImpl.class})
 public class ServerConfig {
+
+    @Bean
+    public Sampler sampler() {
+        return new AlwaysSampler();
+    }
 }
